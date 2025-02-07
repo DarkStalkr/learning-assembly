47dd5e311136026a773430eb8c962db34c785c47
YourDarkStalker <diego.d_correa@hotmail.com>
2025-02-07 00:59:26 -0600

    Added first .asm file

---
+++ asem.s
@@ -0,0 +1,20 @@
+.global _start
+.intel_syntax noprefix
+
+_start:
+  
+ // sys write 
+  mov rax, 1
+  mov rdi, 1
+  lea rsi, [hello_world]
+  mov rdx, 14
+  syscall
+
+  // sys exit
+  mov rax, 60
+  mov rdi, 69
+  syscall
+
+
+hello_world:
+  .asciz "Hello, Github!\n"
